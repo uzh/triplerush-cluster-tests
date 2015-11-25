b26c7ffc731f6d6f23cc011c377775ac65e4f5ea
Akanksha Singh <akanksa.singh@ihealthtechnologies.com>
2015-11-25 18:01:24 +0530

    removing unnecessary reference.conf file from resources

--- src/multi-jvm/scala/com/triplerush/ClusterNodeProvisionerSpec.scala
+++ src/multi-jvm/scala/com/triplerush/ClusterNodeProvisionerSpec.scala
@@ -7,6 +7,7 @@ import akka.remote.testkit.{MultiNodeConfig, MultiNodeSpec, MultiNodeSpecCallbac
 import akka.testkit.ImplicitSender
 import akka.util.Timeout
 import com.signalcollect.nodeprovisioning.cluster.{ClusterNodeProvisionerActor, RetrieveNodeActors}
+import com.triplerush.ClusterPageRankConfig._
 import com.typesafe.config.ConfigFactory
 import org.scalatest._
 import org.scalatest.concurrent.ScalaFutures
@@ -22,36 +23,60 @@ class ClusterSpecMultiJvmNode2 extends ClusterNodeProvisionerSpec
 class ClusterSpecMultiJvmNode3 extends ClusterNodeProvisionerSpec
 
 
-object MultiNodeTestConfig extends MultiNodeConfig {
-  val master = role("master")
+object ClusterNodeProvisionerConfig extends MultiNodeConfig {
+  val provisioner = role("provisioner")
   val worker1 = role("worker1")
   val worker2 = role("worker2")
-
-  val nodeConfig = ConfigFactory.load()
-  val seedIp = nodeConfig.getString("akka.clustering.seed-ip")
-  val seedPort = nodeConfig.getInt("akka.clustering.seed-port")
+  val seedPort = 2558
   val clusterName = "ClusterNodeProvisionerSpec"
 
-  nodeConfig(master) {
-    ConfigFactory.parseString(s"akka.remote.netty.tcp.port=$seedPort")
+  nodeConfig(provisioner) {
+    MultiJvmConfig.provisionerCommonConfig(seedPort)
   }
   // this configuration will be used for all nodes
   // note that no fixed host names and ports are used
-  commonConfig(ConfigFactory.parseString( s"""akka.cluster.seed-nodes=["akka.tcp://"${clusterName}"@"${seedIp}":"${seedPort}]""")
-    .withFallback(ConfigFactory.load()))
+  commonConfig {
+    val mappingsConfig =
+      """akka.actor.kryo.mappings {
+        |  "com.triplerush.ModularAggregator" = 133,
+        |    }""".stripMargin
+
+    val largeTimeoutConfig =
+      """
+        |akka {
+        |  testconductor {
+        |
+        |    # Timeout for joining a barrier: this is the maximum time any participants
+        |    # waits for everybody else to join a named barrier.
+        |    barrier-timeout = 100s
+        |
+        |    # Timeout for interrogation of TestConductor’s Controller actor
+        |    query-timeout = 20s
+        |
+        |    # amount of time for the ClientFSM to wait for the connection to the conductor
+        |    # to be successful
+        |    connect-timeout = 60s
+        |
+        |    # Number of connect attempts to be made to the conductor controller
+        |    client-reconnects = 20
+        |    }
+        |}
+      """.stripMargin
+    ConfigFactory.parseString(largeTimeoutConfig).withFallback(MultiJvmConfig.nodeCommonConfig(clusterName, seedPort, mappingsConfig))
+  }
 }
 
-class ClusterNodeProvisionerSpec extends MultiNodeSpec(MultiNodeTestConfig) with STMultiNodeSpec
+class ClusterNodeProvisionerSpec extends MultiNodeSpec(ClusterNodeProvisionerConfig) with STMultiNodeSpec
 with ImplicitSender with ScalaFutures {
 
-  import MultiNodeTestConfig._
+  import ClusterNodeProvisionerConfig._
 
   override def initialParticipants = roles.size
 
   implicit override val patienceConfig =
     PatienceConfig(timeout = scaled(Span(300, Seconds)), interval = scaled(Span(1000, Millis)))
 
-  val masterAddress = node(master).address
+  val masterAddress = node(provisioner).address
   val worker1Address = node(worker1).address
   val worker2Address = node(worker2).address
   val workers = 3
@@ -61,7 +86,7 @@ with ImplicitSender with ScalaFutures {
 
   "SignalCollect" should {
     "support setting the number of workers created on each node" in {
-      runOn(master) {
+      runOn(provisioner) {
         system.actorOf(Props(classOf[ClusterNodeProvisionerActor], idleDetectionPropagationDelayInMilliseconds,
           "ClusterMasterBootstrap", workers), "ClusterMasterBootstrap")
       }
@@ -77,9 +102,9 @@ with ImplicitSender with ScalaFutures {
       }
       enterBarrier("worker2 started")
 
-      runOn(master) {
+      runOn(provisioner) {
         implicit val timeout = Timeout(300.seconds)
-        val masterActor = system.actorSelection(node(master) / "user" / "ClusterMasterBootstrap")
+        val masterActor = system.actorSelection(node(provisioner) / "user" / "ClusterMasterBootstrap")
         val nodeActorsFuture = (masterActor ? RetrieveNodeActors).mapTo[Array[ActorRef]]
         whenReady(nodeActorsFuture) { nodeActors =>
           assert(nodeActors.size == workers)
